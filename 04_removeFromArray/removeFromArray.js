5e95b327563f018004c9af20f64a90b1df95491b
13011brett <scarlett.brett123@gmail.com>
2023-02-21 13:38:13 -0500

    Tougher! Rem from array done.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+
+
+const removeFromArray = function(array, ...params) {
+    var newArray = [];
+
+
+
+
+    array.forEach(function myFunct(item){
+        var isRemoved = false;
+        for(var i = 0; i < params.length; i++){
+            if (item === params[i]){
+                isRemoved = true;
+            }
+
+        }
+        if(!isRemoved){
+            newArray.push(item);
+        }
+    })
+
+    
+    return newArray;
 
 };
 
