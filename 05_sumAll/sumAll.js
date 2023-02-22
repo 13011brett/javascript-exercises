8aaaa9a52076bce0af70f88170da8e5bd13bb493
13011brett <scarlett.brett123@gmail.com>
2023-02-21 22:17:17 -0500

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(var1, var2) {
+    var greater = (var1 > var2) ? var1 : var2;
+    var lesser = (var1 < var2) ? var1 : var2;
+    var culmative = 0;
+    
+    if(typeof(greater) != 'number' || typeof(lesser) != 'number' 
+        || greater <= 0 || lesser <= 0) return 'ERROR';
+    
+    for( ; lesser <= greater; lesser++){
+        culmative += lesser;
+        
+        
+    }
+    return culmative;
+
 
 };
 
