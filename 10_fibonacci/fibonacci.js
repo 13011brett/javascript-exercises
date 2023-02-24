8cd9366ad0e6e49e96ce6f7a2abc28b1d7fcc53d
13011brett <scarlett.brett123@gmail.com>
2023-02-24 14:40:16 -0500

    Fibonacci (simple) done.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    fibNum = Number(fibNum);
+    var nextNumber;
+    firstNum = 0;
+    secondNum = 1;
+    nextNumber = (fibNum <= 2 && fibNum > 0) ? 1 : "OOPS";  
+    for(var i = 1; i < fibNum; i++){
+        nextNumber = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = nextNumber;
+    }
+    return nextNumber;
+     
 
 };
 
