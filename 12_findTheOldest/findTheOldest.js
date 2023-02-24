02095ee887d63560395bf34afb833356822bbc56
13011brett <scarlett.brett123@gmail.com>
2023-02-24 16:34:31 -0500

    Little change made, fun to see how sort works

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,11 @@
 const findTheOldest = function(people) {
-    const currentYear = 2023;
+    const currentYear = new Date().getFullYear();
     const oldest = people.sort(function(a, b){
         if(b.yearOfDeath == undefined) b.yearOfDeath = currentYear;
         const lastGuy = a.yearOfDeath - a.yearOfBirth;
         const nextGuy = b.yearOfDeath - b.yearOfBirth;
-        return lastGuy > nextGuy ? -1 : 1;
+        return nextGuy - lastGuy;
+        // return lastGuy > nextGuy ? -1 : 1;
     });
 
     return oldest.find(x => x.name);
