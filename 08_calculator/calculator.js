5d45331fb7610102b2748106f6db85c8e8947556
13011brett <scarlett.brett123@gmail.com>
2023-02-24 13:33:45 -0500

    Basic Calcs done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,36 @@
-const add = function() {
+const add = function(num1, num2) {
+  return 0 + num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return 0 + num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(nums = []) {
+  return nums.length <= 0 ? 0 :  nums.reduce((a,b) => a+b )
+  
 	
 };
 
-const multiply = function() {
+const multiply = function(nums = []) {
+  return nums.length <= 0 ? 0 :  nums.reduce((a,b) => a*b )
 
 };
 
-const power = function() {
+const power = function(num, powerTo) {
+  return num <= 0  ? 1 : Math.pow(num, powerTo)  ;
 	
 };
 
-const factorial = function() {
+const factorial = function(factor) {
+  result = 1;
+
+  for(let i = 1; i <= factor; i++ ){
+    result = result*i
+  }
+  return result;
 	
 };
 
